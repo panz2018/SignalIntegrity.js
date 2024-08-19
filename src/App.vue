8ebbf4aacd21d7adfb632ce4e57bc341995a9e8c
Zeyu Pan <panzeyu@gmail.com>
2024-08-18 20:25:14 -0400

    Add container for logicflow

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <LogicFlow msg="You did it!" />
+  <LogicFlow class="main" />
 </template>
 
 <script setup lang="ts">
 import LogicFlow from './components/LogicFlow.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.main {
+  position: absolute;
+  width: 100%;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  overflow-y: auto;
+}
+</style>
--- src/components/LogicFlow.vue
+++ src/components/LogicFlow.vue
@@ -1,41 +1,15 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <div class="container" ref="container"></div>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+const container = ref(null)
+</script>
+
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.container {
+  width: 100%;
+  height: 100%;
 }
 </style>
