f127c35749dbbe0df2e523b8998377c225878837
Zeyu Pan <panzeyu@gmail.com>
2024-08-24 10:49:31 -0400

    click event for relation-graph

--- src/components/FlowGraph.vue
+++ src/components/FlowGraph.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <div class="main">
-      <relation-graph ref="graphRef" :options="options" />
+      <relation-graph ref="graph" :options="options" :on-node-click="onNodeClick">
+        <template #node="{ node }">
+          <div style="padding-top: 20px">Node: {{ node.text }}</div>
+        </template>
+      </relation-graph>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import RelationGraph, { type RGOptions } from 'relation-graph-vue3'
-const graphRef = ref<RelationGraph>()
+import RelationGraph, { type RGOptions, type RGNode } from 'relation-graph/vue3'
+const graph = ref<RelationGraph>()
 const options: RGOptions = {
   defaultExpandHolderPosition: 'right'
-  // defaultLineShape: 4,
+  // defaultLineShape: 4
   // debug: true,
   // showDebugPanel: true
 }
-const jsonData = {
+const onNodeClick = (node: RGNode) => {
+  console.log('onNodeClick:', node.id)
+  return true
+}
+const data = {
   rootId: 'a',
   nodes: [
     { id: 'a', text: 'a' },
@@ -27,28 +35,24 @@ const jsonData = {
     { id: 'f', text: 'f' }
   ],
   lines: [
-    { from: 'a', to: 'b' },
-    { from: 'a', to: 'c' },
-    { from: 'a', to: 'd' },
-    { from: 'a', to: 'e' },
-    { from: 'a', to: 'f' }
+    { from: 'a', to: 'b', text: 'a -> b' },
+    { from: 'a', to: 'c', text: 'a -> c' },
+    { from: 'a', to: 'd', text: 'a -> d' },
+    { from: 'a', to: 'e', text: 'a -> e' },
+    { from: 'a', to: 'f', text: 'a -> f' }
   ]
 }
-onMounted(() => {
-  // The node and line in the above data can refer to the options in "Node" and "Link & Line" for configuration.
-  // Node: https://www.relation-graph.com/#/docs/node
-  // Link & Line: https://www.relation-graph.com/#/docs/link
-  if (graphRef.value !== undefined) {
-    graphRef.value.setJsonData(jsonData)
+onMounted(async () => {
+  if (graph.value !== undefined) {
+    graph.value.setJsonData(data)
+    // const graphInstance = graph.value.getInstance()
+    // graphInstance.addNodes(data.nodes)
+    // graphInstance.addLines(data.lines)
+    // graphInstance.rootNode = graphInstance.getNodeById(data.rootId)
+    // await graphInstance.doLayout() // Layout using the layouter set in graphOptions
+    // await graphInstance.moveToCenter() // Find the center based on node distribution and center the view
+    // await graphInstance.zoomToFit() // Zoom to fit, so that all nodes can be displayed in the visible area
   }
-  // The graphRef$.value.setJsonData(jsonData, callback) method is a convenient method that is equivalent to the following code:
-  //  const graphInstance = graphRef$.value.getInstance();
-  //  graphInstance.addNodes(jsonData.nodes);
-  //  graphInstance.addLines(jsonData.lines);
-  //  graphInstance.rootNode = graphInstance.getNodeById(jsonData.rootId);
-  //  await graphInstance.doLayout(); // Layout using the layouter set in graphOptions
-  //  await graphInstance.moveToCenter(); // Find the center based on node distribution and center the view
-  //  await graphInstance.zoomToFit(); // Zoom to fit, so that all nodes can be displayed in the visible area
 })
 </script>
 
