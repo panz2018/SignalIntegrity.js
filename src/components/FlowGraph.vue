1322625a19814509fe9902810fd1d01e87d546d2
Zeyu Pan <panzeyu@gmail.com>
2024-08-24 11:07:44 -0400

    replace relation-graph with vue-flow

--- src/components/FlowGraph.vue
+++ src/components/FlowGraph.vue
@@ -1,64 +1,50 @@
 <template>
-  <div>
-    <div class="main">
-      <relation-graph ref="graph" :options="options" :on-node-click="onNodeClick">
-        <template #node="{ node }">
-          <div style="padding-top: 20px">Node: {{ node.text }}</div>
-        </template>
-      </relation-graph>
-    </div>
-  </div>
+  <div><VueFlow :nodes="nodes" :edges="edges"></VueFlow></div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import RelationGraph, { type RGOptions, type RGNode } from 'relation-graph/vue3'
-const graph = ref<RelationGraph>()
-const options: RGOptions = {
-  defaultExpandHolderPosition: 'right'
-  // defaultLineShape: 4
-  // debug: true,
-  // showDebugPanel: true
-}
-const onNodeClick = (node: RGNode) => {
-  console.log('onNodeClick:', node.id)
-  return true
-}
-const data = {
-  rootId: 'a',
-  nodes: [
-    { id: 'a', text: 'a' },
-    { id: 'b', text: 'b' },
-    { id: 'c', text: 'c' },
-    { id: 'd', text: 'd' },
-    { id: 'e', text: 'e' },
-    { id: 'f', text: 'f' }
-  ],
-  lines: [
-    { from: 'a', to: 'b', text: 'a -> b' },
-    { from: 'a', to: 'c', text: 'a -> c' },
-    { from: 'a', to: 'd', text: 'a -> d' },
-    { from: 'a', to: 'e', text: 'a -> e' },
-    { from: 'a', to: 'f', text: 'a -> f' }
-  ]
-}
-onMounted(async () => {
-  if (graph.value !== undefined) {
-    graph.value.setJsonData(data)
-    // const graphInstance = graph.value.getInstance()
-    // graphInstance.addNodes(data.nodes)
-    // graphInstance.addLines(data.lines)
-    // graphInstance.rootNode = graphInstance.getNodeById(data.rootId)
-    // await graphInstance.doLayout() // Layout using the layouter set in graphOptions
-    // await graphInstance.moveToCenter() // Find the center based on node distribution and center the view
-    // await graphInstance.zoomToFit() // Zoom to fit, so that all nodes can be displayed in the visible area
+import { ref } from 'vue'
+
+// Setup the VueFlow
+import { VueFlow } from '@vue-flow/core'
+import type { Node, Edge } from '@vue-flow/core'
+const nodes = ref<Node[]>([
+  {
+    id: '1',
+    type: 'input',
+    position: { x: 250, y: 5 },
+    data: { label: 'Node 1' }
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'Node 2' }
+  },
+  {
+    id: '3',
+    type: 'output',
+    position: { x: 400, y: 200 },
+    data: { label: 'Node 3' }
   }
-})
+])
+const edges = ref<Edge[]>([
+  {
+    id: 'e1->2',
+    source: '1',
+    target: '2'
+  },
+  {
+    id: 'e2->3',
+    source: '2',
+    target: '3',
+    animated: true
+  }
+])
 </script>
 
-<style scoped>
-.main {
-  height: calc(100% - 4px);
-  width: calc(100%);
-}
+<style scoped></style>
+
+<style>
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
 </style>
