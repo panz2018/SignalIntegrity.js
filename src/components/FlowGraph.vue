d095fe695dba9bce357fcc6210353bed8fcf6554
Zeyu Pan <panzeyu@gmail.com>
2024-08-25 11:04:34 -0400

    Add background and minimap

--- src/components/FlowGraph.vue
+++ src/components/FlowGraph.vue
@@ -1,5 +1,17 @@
 <template>
-  <div><VueFlow :nodes="nodes" :edges="edges"></VueFlow></div>
+  <VueFlow
+    :nodes="nodes"
+    :edges="edges"
+    :default-viewport="{ zoom: 1.0 }"
+    :min-zoom="0.2"
+    :max-zoom="4"
+  >
+    <Background pattern-color="#AAAAAA" :gap="16" />
+    <MiniMap />
+    <Panel position="top-right">
+      <button type="button" @click="addNode">Add a node</button>
+    </Panel>
+  </VueFlow>
 </template>
 
 <script setup lang="ts">
@@ -13,12 +25,15 @@ const nodes = ref<Node[]>([
     id: '1',
     type: 'input',
     position: { x: 250, y: 5 },
-    data: { label: 'Node 1' }
+    data: { label: 'Node 1' },
+    style: { color: 'white', backgroundColor: 'green', width: '100px', height: '50px' }
   },
   {
     id: '2',
+    type: 'default',
     position: { x: 100, y: 100 },
-    data: { label: 'Node 2' }
+    data: { label: 'Node 2' },
+    style: { '--vf-node-color': 'blue' }
   },
   {
     id: '3',
@@ -31,15 +46,33 @@ const edges = ref<Edge[]>([
   {
     id: 'e1->2',
     source: '1',
-    target: '2'
+    target: '2',
+    // type: 'bezier',
+    animated: false
   },
   {
     id: 'e2->3',
     source: '2',
     target: '3',
+    // type: 'bezier',
     animated: true
   }
 ])
+
+// Setup panel for VueFlow
+import { Panel } from '@vue-flow/core'
+function addNode() {
+  const id = Date.now().toString()
+  nodes.value.push({
+    id: id,
+    position: { x: 150, y: 50 },
+    data: { label: `Node ${id}` }
+  })
+}
+
+// Setup background, minimap
+import { Background } from '@vue-flow/background'
+import { MiniMap } from '@vue-flow/minimap'
 </script>
 
 <style scoped></style>
