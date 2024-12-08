d0ce8dda291660a60890097b7cb3e033f190378a
panz2018 <49798383+panz2018@users.noreply.github.com>
2024-12-08 15:19:53 -0500

    Correct vitest

--- src/FlowGraph/NewFile/NewFile.test.ts
+++ src/FlowGraph/NewFile/NewFile.test.ts
@@ -51,10 +51,10 @@ describe.concurrent('NewFile.ts', () => {
     // Check previous flow graph
     expect(flow.getNodes.value.length).toBe(2)
     expect(flow.getEdges.value.length).toBe(1)
-    expect(flow.id).not.toBe(flows.current)
+    expect(flow.id).not.toBe(flows.current.toString())
     // Check the new flow graph
     const newFlow = useVueFlow(flows.current.toString())
-    expect(newFlow.id).toBe(flows.current)
+    expect(newFlow.id).toBe(flows.current.toString())
     expect(newFlow.getNodes.value).toStrictEqual([])
     expect(newFlow.getEdges.value).toStrictEqual([])
   })
